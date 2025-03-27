c02b91015784f046f077a0b936ade1068fc4f1e7
Alexandr_Shmidt <alexandr_shmidt98@vk.com>
2025-03-27 18:13:44 +0700

    Решение

---
+++ answer.php
@@ -0,0 +1,12 @@
+<?php
+function pairNumbersSearcher($array)  {
+    $arrayFilter = array_filter($array, 'is_int');
+    $dropKey = array_values($arrayFilter);
+    $result = [];
+    for($i = 0; $i < count($dropKey) -  1; $i++) {
+        if($dropKey[$i] === $dropKey[$i + 1] - 1) {
+            $result[] = [$dropKey[$i], $dropKey[$i + 1]];
+        }
+    }
+    return $result;
+}
